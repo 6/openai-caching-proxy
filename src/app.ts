6a66e509df6882cb28266a2a567a93dfd16ba9c1
Peter Graham <peterghm@gmail.com>
2022-12-10 17:36:12 -0500

    basic working cache

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
+import {
+  createProxyMiddleware,
+  Filter,
+  Options,
+  RequestHandler,
+  responseInterceptor,
+} from 'http-proxy-middleware';
 import morgan from 'morgan';
+import { getCacheKey, readCache, writeCache } from './cache';
 
 const app: Application = express();
 
@@ -12,6 +19,33 @@ app.get('/', async (req: Request, res: Response) => {
   res.json({ hello: 'hola' });
 });
 
+const checkCache = async (req: Request, res: Response, next: NextFunction) => {
+  const cacheKey = getCacheKey({
+    authHeader: req.headers['authorization'],
+    body: req.body,
+    method: req.method,
+    path: req.path,
+  });
+  res.locals.cacheKey = cacheKey;
+
+  console.log('Checking cache for:', { method: req.method, path: req.path, cacheKey });
+
+  const cachedResponse = await readCache({ cacheKey });
+  if (cachedResponse) {
+    console.log('Returning cached resopnse.');
+    for (const [key, value] of Object.entries(cachedResponse.headers)) {
+      res.setHeader(key, value);
+    }
+    return res.status(cachedResponse.status).send(cachedResponse.body);
+  }
+
+  console.log('No cache found, proxying request to api.openai.com instead.');
+
+  next();
+};
+
+app.use(checkCache);
+
 // Sample (easy) request to try:
 // curl http://localhost:3001/proxy/models -H 'Authorization: Bearer YOUR_API_KEY'
 app.use(
@@ -20,6 +54,28 @@ app.use(
     target: 'https://api.openai.com/v1',
     changeOrigin: true,
     pathRewrite: { '^/proxy': '' },
+    selfHandleResponse: true,
+    onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+      const response = responseBuffer.toString('utf8'); // convert buffer to string
+      console.log(response.substring(0, 25), '...'); // log response body
+
+      const cacheKey = (res as any).locals?.cacheKey; // This type is incorrect?
+      if (cacheKey && res.statusCode >= 200 && res.statusCode < 300 && response?.length) {
+        console.log('Writing 2xx response to cache: ', cacheKey);
+        await writeCache({
+          cacheKey,
+          response: {
+            headers: res.getHeaders() as Record<string, string>,
+            body: response,
+            status: res.statusCode,
+          },
+        });
+      } else {
+        console.log('Not caching error or empty response.');
+      }
+
+      return responseBuffer;
+    }),
   }),
 );
 
--- src/cache.ts
+++ src/cache.ts
@@ -1,5 +1,18 @@
 import { identity, pickBy } from 'lodash';
 import objectHash from 'object-hash';
+import { createClient } from 'redis';
+
+type RedisClient = ReturnType<typeof createClient>;
+let _client: RedisClient;
+const getClient = async (): Promise<RedisClient> => {
+  if (_client) {
+    return _client;
+  }
+  const client = createClient();
+  await client.connect();
+  _client = client;
+  return client;
+};
 
 interface RequestProps {
   method: string;
@@ -8,8 +21,38 @@ interface RequestProps {
   body: string | undefined;
 }
 
+interface CachedResponse {
+  body: string;
+  headers: Record<string, string>;
+  status: number;
+}
+
 export const getCacheKey = (props: RequestProps): string => {
   const propsWithoutUndefined = pickBy(props, identity);
   const hash = objectHash(propsWithoutUndefined);
   return hash;
+};
+
+export const readCache = async ({
+  cacheKey,
+}: {
+  cacheKey: string;
+}): Promise<CachedResponse | null> => {
+  const client = await getClient();
+  const result = await client.get(cacheKey);
+  if (result) {
+    return JSON.parse(result) as CachedResponse;
+  }
+  return null;
+};
+
+interface WriteCacheProps {
+  cacheKey: string;
+  response: CachedResponse;
+}
+
+export const writeCache = async ({ cacheKey, response }: WriteCacheProps): Promise<void> => {
+  const client = await getClient();
+  await client.set(cacheKey, JSON.stringify(response));
+  return;
 };
