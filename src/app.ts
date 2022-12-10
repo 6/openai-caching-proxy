d438d48a900cba71477cf85f6345570e9b386abf
Peter Graham <peterghm@gmail.com>
2022-12-10 16:39:35 -0500

    basic working proxy

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,36 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
+import morgan from 'morgan';
 
 const app: Application = express();
 
 app.use(cors());
+app.use(morgan('dev'));
 
 app.get('/', async (req: Request, res: Response) => {
   res.json({ hello: 'hola' });
 });
 
+app.use(
+  '/proxy',
+  createProxyMiddleware({
+    target: 'https://api.openai.com/v1',
+    changeOrigin: true,
+    pathRewrite: { '^/proxy': '' },
+  }),
+);
+
+// app.use(
+//   '/api',
+//   createProxyMiddleware({
+//     target: 'http://www.example.org/api',
+//     changeOrigin: true,
+//   }),
+// );
+
 const port: number = Number(process.env.PORT || 3001);
 
-const server = app.listen(port, function () {
+app.listen(port, function () {
   console.log(`App is listening on port ${port} !`);
 });
-
-// Set a long timeout globally for now, some non-streaming
-// responses can take a while:
-server.setTimeout(Number(process.env.SERVER_TIMEOUT || 60));
